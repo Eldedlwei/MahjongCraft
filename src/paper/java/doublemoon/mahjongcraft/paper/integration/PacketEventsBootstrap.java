0dfa6f4dd88b4be6198468af4e8e5f055c4a3b2b
Eldedlwei <Eldedlwei@users.noreply.github.com>
2026-02-20 12:18:07 +0800

    Fix PacketEvents builder class

--- src/paper/java/doublemoon/mahjongcraft/paper/integration/PacketEventsBootstrap.java
+++ src/paper/java/doublemoon/mahjongcraft/paper/integration/PacketEventsBootstrap.java
@@ -16,15 +16,7 @@ public final class PacketEventsBootstrap {
             return false;
         }
         try {
-            Class<?> builderClass = null;
-            try {
-                builderClass = Class.forName("com.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder");
-            } catch (ClassNotFoundException ignored) {
-                // Newer PacketEvents versions moved builders under the Bukkit package.
-            }
-            if (builderClass == null) {
-                builderClass = Class.forName("com.github.retrooper.packetevents.factory.bukkit.BukkitPacketEventsBuilder");
-            }
+            Class<?> builderClass = Class.forName("com.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder");
             Method build = builderClass.getMethod("build", org.bukkit.plugin.Plugin.class);
             Object api = build.invoke(null, plugin);
 
